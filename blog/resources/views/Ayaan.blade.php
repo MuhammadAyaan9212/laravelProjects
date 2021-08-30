50b533b95c0d9343163a48a4cfc8f37bb3d60fb9
MuhammadAyaan9212 <ayaanpeera80@gmail.com>
2021-08-30 22:25:37 +0500

    all laravel projects

---
+++ blog/resources/views/Ayaan.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Ayaan</title>
+  </head>
+  <body>
+    <h1 class="text-warning">Ayaan</h1>
+    <h1 class="text-success">Faisal</h1>
+    <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+  </body>
+</html>
